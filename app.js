47fd39f8c5addce0ea125b508609821eb4c02a50
Tanel Märjama <tanel.marjama@khk.ee>
2022-11-29 23:07:47 +0200

    Added socket.io for event listening

--- app.js
+++ app.js
@@ -2,14 +2,40 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const queries = require('./queries')
-
+const http = require('http');
+const { Server } = require("socket.io");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//http
+const server = http.createServer(app)
 app.use(cors({
     origin: '*'
 }));
 
+const port = process.env.PORT || 3001;
+server.listen(3001, () => {
+    console.log(`Server started on port: ${port}`);
+});
+
+//ws
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    socket.onAny(() => {
+        io.emit('event')
+    });
+});
 
 app.get('/', queries.getTodos)
 app.post('/', queries.createTodo)
